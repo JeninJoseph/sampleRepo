a16ff7ea22028f662ea502e0510179055c0b94d4
Jenin Joseph <jenin.jk@gmail.com>
2024-07-11 12:20:25 +0530

    Trail Run

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,9 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('loginToSauce', () => {
-    cy.get('[data-test="username"]').type('standard_user');
-    cy.get('[data-test="password"]').type('secret_sauce');
-    cy.get('[data-test="login-button"]').click();
-})
-import 'cypress-file-upload'; 
